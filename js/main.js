47ee0d3ea2aaff514152fb1b219248cb4ec0e0ae
nadjib <nadjib.programmation1@gmail.com>
2021-09-18 23:05:08 +0100

    upload the design

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const linkbtn = document.querySelector("link-btn");
+const links = document.querySelector("links");
+linkbtn.addEventListener("click", (e) => {
+    icon.classList.toggle("show-more");
+    if (linkbtn.innerHTML ==="<span class="icon">
+    <span></span>
+    <span></span>
+    <span></span>
+</span>")
+}) 
